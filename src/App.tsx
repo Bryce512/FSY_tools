18cef3a375d3435dccded061bf4b0d36c0c47a4e
Bryce512 <bryce.whitworth@gmail.com>
2025-03-10 16:45:28 -0600

    test again

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>FSY Tools + assignment tool</h1>
+      <h1>FSY Tools + assignments</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
