532d4e8cf687436f2b65f2fde8465f611b2bc4f6
Bryce512 <bryce.whitworth@gmail.com>
2025-03-10 14:54:53 -0600

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/FSY_tools/", // Change this to your repo name
   plugins: [react()],
-})
+});
