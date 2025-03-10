b63ef0ce1d33237bcb5020cc5e8b1465a6ee6ae1
Bryce512 <121685793+Bryce512@users.noreply.github.com>
2025-03-10 22:43:18 +0000

    Deploying to main from @ Bryce512/FSY_tools@d79370e0d2fa29cc82d82c1f6fd07c0e1325693f 🚀

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: "/FSY_tools/", // Change this to your repo name
-  plugins: [react()],
-});
