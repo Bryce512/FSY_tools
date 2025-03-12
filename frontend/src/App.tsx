9896caa8d94b2e916c498609b47ecb6ca97a20ef
Bryce512 <bryce.whitworth@gmail.com>
2025-03-12 09:31:08 -0600

    add router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import "./components/login/Login";
 import MaxLogin from "./components/login/Login";
 
 function App() {
   return (
     <>
-      <div>
-        <MaxLogin />
-      </div>
+    <Routes>
+      <Route path="/" element={<MaxLogin />} />
+    </Routes>
     </>
   );
 }
