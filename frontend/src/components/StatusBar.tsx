a33fc46ecf2199045bc22ef06ca2b42a961488be
Bryce512 <bryce.whitworth@gmail.com>
2025-03-12 08:52:46 -0600

    create front/backend dir

--- frontend/src/components/StatusBar.tsx
+++ frontend/src/components/StatusBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import styles from "./login/Login.module.css";
+import styles from "./MaxLogin.module.css";
 
 const StatusBar: React.FC = () => {
   return (
