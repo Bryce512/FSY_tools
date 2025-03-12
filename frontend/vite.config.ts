20071bfeb58a60ed10735e0788a5ddf7c945ca87
Bryce512 <bryce.whitworth@gmail.com>
2025-03-12 10:24:24 -0600

    try again

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,12 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/FSY_tools/", // Change this to your repo name
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: "index.html",
-        notFound: "404.html", // Ensures 404.html is created
-      },
-    },
-  }
 });
