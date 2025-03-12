946ca048d9289faa1eae0b1b0aebb1e1d3794941
Thumperr <asdf;lkj@>
2025-03-12 09:28:40 -0600

    initial builder.io import

---
+++ frontend/src/components/AddTestimonyRoom/InputDesign.tsx
@@ -0,0 +1,26 @@
+"use client";
+import React from "react";
+import styles from "./InputDesign.module.css";
+import StatusBar from "./StatusBar";
+import NavBar from "./NavBar";
+import RoomForm from "./RoomForm";
+import BottomNav from "./BottomNav";
+
+function InputDesign() {
+  return (
+    <>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&family=Roboto:wght@400;500&display=swap"
+        rel="stylesheet"
+      />
+      <main className={styles.div}>
+        <StatusBar />
+        <NavBar />
+        <RoomForm />
+        <BottomNav />
+      </main>
+    </>
+  );
+}
+
+export default InputDesign;
