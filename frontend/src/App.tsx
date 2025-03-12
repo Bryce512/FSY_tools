998b752917bedeedf7665a1ae5e95cacc98188f2
Bryce512 <bryce.whitworth@gmail.com>
2025-03-12 08:52:46 -0600

    create front/backend dir

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,13 @@ import './components/MaxLogin'
 import MaxLogin from './components/MaxLogin'
 import AssignTestimonyRooms from './components/AssignTestimonyRooms'
 
-
-
 function App() {
   return (
     <>
       <div>
         <MaxLogin />
         <AssignTestimonyRooms/>
+        <MaxLogin/>
       </div>
     </>
   );
