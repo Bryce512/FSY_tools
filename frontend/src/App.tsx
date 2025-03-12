1f41799054fdf9e17740d766560213728bdf7b95
Bryce512 <bryce.whitworth@gmail.com>
2025-03-12 09:59:50 -0600

    add router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,14 @@
-import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import "./components/login/Login";
 import MaxLogin from "./components/login/Login";
+import InputDesign from "./components/AddTestimonyRoom/InputDesign";
+import "./App.css"; // Keep styles
 
 function App() {
   return (
-    <>
     <Routes>
       <Route path="/" element={<MaxLogin />} />
+      <Route path="/TestimonyRooms" element={<InputDesign />} />
     </Routes>
-    </>
   );
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <BrowserRouter basename="/FSY_tools">
+      <App />
+    </BrowserRouter>
+  );
+} else {
+  console.error("Failed to find the root element");
+}
