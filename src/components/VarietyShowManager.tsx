d8b99067e78831e86a0234254d98e70e72f4a8e8
teaganann <113033017+teaganann@users.noreply.github.com>
2025-03-15 17:41:13 -0600

    creating variety show view

---
+++ src/components/VarietyShowManager.tsx
@@ -0,0 +1,26 @@
+'use client';
+import React from 'react';
+import styles from './VarietyShowManager.module.css';
+import { StatusBar } from './VarietyShow/StatusBar';
+import { NavigationHeader } from './VarietyShow/NavigationHeader';
+import VarietyShowBody from './VarietyShow/VarietyShowBody';
+import { BottomNavigation } from './VarietyShow/BottomNavigation';
+
+export const VarietyShowManager: React.FC = () => {
+  return (
+    <main className={styles.container}>
+      <StatusBar />
+      <NavigationHeader />
+      <section className={styles.content}>
+        <div className={styles.actionButtons}>
+          <button className={styles.addButton}>+</button>
+          <button className={styles.publishButton}>Publish</button>
+        </div>
+        <VarietyShowBody />
+      </section>
+      <BottomNavigation />
+    </main>
+  );
+};
+
+export default VarietyShowManager;
