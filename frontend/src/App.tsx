1f3ce6a4dcebf255d350e8585adb5283ef4091dc
Bryce512 <bryce.whitworth@gmail.com>
2025-03-17 09:32:46 -0600

    add teagan

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -32,6 +32,7 @@ import SongRequestForm from "./components/RequestSong/SongRequestForm";
 import RoomForm from "./components/AddTestimonyRoom/RoomForm";
 import BryceAddCounselor from "./components/AddCounselor/AddCounselor";
 import LoginForm from "./components/login/LoginForm";
+import VarietyShowManager from "./components/VarietyShowManager";
 
 function App() {
   return (
@@ -44,6 +45,8 @@ function App() {
       <Route path="/RoomForm" element={<RoomForm />} />
       <Route path="/AddCounselor" element={<BryceAddCounselor />} />
       <Route path="/Login" element={<LoginForm />} />
+      <Route path="/VarietyShow" element={<VarietyShowManager />} />
+      <Route path="*" element={<h1>Not Found</h1>} />
     </Routes>
 <<<<<<< HEAD
 >>>>>>> f3981e1 (add router)
