83bcd9b8786a1e16bf05f245a22d3258b48b0b94
JonasButikofer <brojobutikofer@gmail.com>
2025-03-19 23:00:19 -0600

    Database scaffolding

---
+++ backend/fsyTools/fsyTools/Models/Room.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace fsyTools.Models
+{
+    public partial class Room
+    {
+        [Key]
+        public int RoomId { get; set; }
+
+        [Required]
+        public string RoomName { get; set; } = null!;
+
+        [Required]
+        public string Building { get; set; } = null!;
+
+        [Required]
+        public string RoomNumber { get; set; } = null!;
+        public int? Capacity { get; set; }
+
+        public DateTime? CreationDate { get; set; }
+    }
+}
---
+++ backend/fsyTools/fsyTools/Models/SongList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fsyTools.Models;
+
+public partial class SongList
+{
+    public int SongId { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Artist { get; set; } = null!;
+
+    public int? UserId { get; set; }
+
+    public DateTime? CreationDate { get; set; }
+
+    public virtual User? User { get; set; }
+}
