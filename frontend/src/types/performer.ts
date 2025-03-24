80b5a810d35a20e17798d09d44809105bb3c6bcb
teaganann <113033017+teaganann@users.noreply.github.com>
2025-03-24 08:57:00 -0600

    sync changes

---
+++ frontend/src/types/performer.ts
@@ -0,0 +1,9 @@
+export interface Performer {
+    show_id : number;
+    first_name: string;
+    last_name : string;
+    act_description : string;
+    team_id: number;
+    order_of_performance: number;
+    is_performing: boolean;
+}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/FSY_tools/", // Change this to your repo name
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
 });
