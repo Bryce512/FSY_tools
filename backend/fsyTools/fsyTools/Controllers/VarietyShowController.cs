0373555d7c4fc6a80434b5f94eabf02804db0491
JonasButikofer <brojobutikofer@gmail.com>
2025-03-26 10:37:13 -0600

    added the server 3000 port to the vite file

--- backend/fsyTools/fsyTools/Controllers/VarietyShowController.cs
+++ backend/fsyTools/fsyTools/Controllers/VarietyShowController.cs
@@ -1,13 +1,1 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-
-namespace fsyTools.Controllers
-{
-    [Route("[controller]")]
-    [ApiController]
-    public class VarietyShowController : ControllerBase
-    {
-       
-
-    }
-}
+﻿
